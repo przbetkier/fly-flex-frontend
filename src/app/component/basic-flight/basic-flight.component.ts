308d043caeac7139d315d1d746a560618b557ca5
Marcin Baranowski <m.baranowskimarcin@gmail.com>
2018-11-18 00:23:00 +0100

    Adds images for cities

--- src/app/component/basic-flight/basic-flight.component.ts
+++ src/app/component/basic-flight/basic-flight.component.ts
@@ -2,13 +2,32 @@ import {Component, Input} from '@angular/core';
 import {Airport} from '../../model/airport';
 
 @Component({
-  selector: 'app-basic-flight',
-  templateUrl: './basic-flight.component.html',
-  styleUrls: ['./basic-flight.component.css']
+    selector: 'app-basic-flight',
+    templateUrl: './basic-flight.component.html',
+    styleUrls: ['./basic-flight.component.css']
 })
 export class BasicFlightComponent {
 
-  @Input()
-  flight: Airport;
+    @Input()
+    flight: Airport;
+
+    private flightImgMapping = {
+        'Kraków': 'krakow',
+        'Warszawa-Modlin': 'warszawa-modlin',
+        'Londyn-Luton': 'londyn',
+        'Londyn-Stansted': 'londyn',
+        'Berlin-Schönefeld': 'berlin',
+        'Barcelona-Girona': 'barcelona',
+        'Lanzarote': 'lancarote',
+        'Alicante': 'alicante',
+        'Malaga': 'malaga',
+        'Gdańsk': 'gdansk'
+    };
+
+    findFlightImg(): string {
+        const imgName = this.flightImgMapping[this.flight.name] || 'default';
+        return '/assets/airports/' + imgName + '.jpg';
+    }
+
 
 }
