471b8cc8bbe9379e4dc36227f619a29064d583c0
Przemysław Betkier <przbetkier@gmail.com>
2018-11-17 13:24:13 +0100

    Homepage and flights-list components init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './component/homepage/homepage.component';
+import {RouterModule} from '@angular/router';
+import { FlightsListComponent } from './component/flights-list/flights-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FlightsListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {path: '', component: HomepageComponent},
+        {path: 'flights', component: FlightsListComponent}
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
